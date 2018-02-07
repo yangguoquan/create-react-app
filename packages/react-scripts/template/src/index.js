f68b486acdc9ecc1e8d10372cd6d9d90a8801824
FDT <guoquan.yang@hkfdt.cn>
2018-02-07 13:55:43 +0800

    更新react版本， 增加eslintrc文件 fix 初始样式

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// pwa 功能
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
